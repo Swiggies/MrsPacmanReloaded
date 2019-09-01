68eac6c58e640eb1743ad61cff2440523c5bf763
Swiggies <38583668+Swiggies@users.noreply.github.com>
2019-09-01 21:17:48 +1000

    New stuff

---
+++ MrsPacmanReloaded/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    [SerializeField] private Texture2D level;
+    [SerializeField] private GameObject wallObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(level.width * 0.5f, (level.height * 0.5f) - 0.5f, -10);
+
+        GetComponent<Camera>().orthographicSize = level.height / 2;
+        for (int x = 0; x < level.width; x++)
+        {
+            for (int y = 0; y < level.height; y++)
+            {
+                if (level.GetPixel(x, y) != Color.white)
+                    Instantiate(wallObj, new Vector2(x, y), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
